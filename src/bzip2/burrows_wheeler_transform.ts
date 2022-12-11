68ec6678666fc86ce37e2800b4d5e7198c6334de
ayazukuri <korscr1027@yahoo.com>
2022-12-11 02:11:43 +0100

    feat: unicode run-length encoding

--- src/bzip2/burrows_wheeler_transform.ts
+++ src/bzip2/burrows_wheeler_transform.ts
@@ -1,3 +1,22 @@
+type SuffixTree = Map<string, SuffixTree>;
+
+export function suffix_tree(input: string): SuffixTree {
+    const stree: SuffixTree = new Map();
+    const reverse_input = input.split("").reverse().join("");
+    for (let i = reverse_input.length; i > 0; i--) {
+        const suffix = reverse_input.slice(i);
+        for (const char of reverse_input) {
+            console.log(char);
+            let subtree = stree;
+            while (subtree.has(char)) subtree = subtree.get(char);
+            subtree.set(char, new Map());
+        }
+    }
+    return stree;
+}
+
 export function burrows_wheeler_transform(input: string): string {
-    
+    const utf32 = ("\x02" + input + "\x03").split("").map(char => char.codePointAt(0));
+    const out = Array(utf32.length);
+    return input;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import { suffix_tree } from "./bzip2/burrows_wheeler_transform";
 import { run_length_enc } from "./bzip2/run_length_enc";
 
-const s = "33333";
-console.log(run_length_enc(s));
+const s = "333eas";
+const t = suffix_tree(s);
+console.log(t.get("s"));
+console.log(t.get("s").get("a"));
