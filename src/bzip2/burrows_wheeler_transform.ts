51b44b2ec4bcf320a4b8d9e5d55c94d84981f54d
ayazukuri <korscr1027@yahoo.com>
2022-12-12 14:05:57 +0100

    feat: suffix array construction from suffix tree

--- src/bzip2/burrows_wheeler_transform.ts
+++ src/bzip2/burrows_wheeler_transform.ts
@@ -1,21 +1,44 @@
-const END = 0x00;
+type END = 0x00;
+const END: END = 0x00;
 
-type SuffixTree = Map<string, SuffixTree | null>;
+type SuffixTreeNode = Map<string | END, SuffixTree>;
+type SuffixTree = SuffixTreeNode | number;
 
 export function suffix_tree(input: string): SuffixTree {
-    const stree: SuffixTree = new Map();
-    for (let i = input.length - 1; i >= 0; i--) {
+    const stree: SuffixTreeNode = new Map();
+    const input_arr: (string | END)[] = input.split("");
+    input_arr.push(END);
+    for (let i = input_arr.length - 1; i >= 0; i--) {
         let subtree = stree;
-        const suffix = input.slice(i);
-        console.log(suffix);
+        const suffix = input_arr.slice(i);
         for (const char of suffix) {
+            if (char === END) {
+                subtree.set(char, i);
+                break;
+            }
             if (!subtree.has(char)) {
                 subtree.set(char, new Map());
             }
-            subtree = subtree.get(char);
+            subtree = <SuffixTreeNode>subtree.get(char);
         }
     }
     return stree;
+}
+
+export function suffix_array(stree: SuffixTree): number[] {
+    const result = [];
+    const stack = [stree];
+    let current_node: SuffixTree;
+    while (current_node = stack.pop()) {
+        const sorted_entries = Array.from((<SuffixTreeNode>current_node).entries())
+        .map<[number, SuffixTree]>(([char, nextree]) => [char === END ? -1 : char.charCodeAt(0), nextree])
+        .sort(([ord, nextree], [ord2, nextree2]) => ord2 - ord);
+        for (const [_, nextree] of sorted_entries) {
+            if (typeof nextree === "number") result.push(nextree);
+            else stack.push(nextree);
+        }
+    }
+    return result;
 }
 
 export function burrows_wheeler_transform(input: string): string {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { suffix_tree } from "./bzip2/burrows_wheeler_transform";
+import { suffix_array, suffix_tree } from "./bzip2/burrows_wheeler_transform";
 import { run_length_enc } from "./bzip2/run_length_enc";
 
-const s = "eaee33$";
+const s = "eaee33";
 const t = suffix_tree(s);
-console.log(t);
+console.log(suffix_array(t));
