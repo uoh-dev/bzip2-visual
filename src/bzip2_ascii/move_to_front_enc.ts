2d2bde16b95385c1dc59b8f11054a599bd5289e1
ayazukuri <korscr1027@yahoo.com>
2022-12-12 15:17:26 +0100

    fix: indexOf bugfix & toArray method added to linked list

--- src/bzip2_ascii/move_to_front_enc.ts
+++ src/bzip2_ascii/move_to_front_enc.ts
@@ -1,6 +1,7 @@
-class LinkedList<T> {
-    head: LinkedListNode<T>;
-    tail: LinkedListNode<T>;
+export class LinkedList<T> {
+    private head: LinkedListNode<T>;
+    private tail: LinkedListNode<T>;
+    private size: number;
 
     constructor(data: T) {
         this.head = {
@@ -9,12 +10,22 @@ class LinkedList<T> {
             next: null
         };
         this.tail = this.head;
+        this.size = 1;
     }
 
     at(i: number): T {
         let list = this.head;
         while (i-- > 0) list = list.next;
         return list.data;
+    }
+
+    indexOf(data: T): number {
+        let i = 0;
+        let node = this.head
+        do {
+            if (node.data === data) return i;
+            i++;
+        } while (node = node.next);
     }
 
     prepend(data: T): void {
@@ -24,6 +35,7 @@ class LinkedList<T> {
             next: this.head
         };
         this.head = this.head.previous;
+        this.size++;
     }
 
     append(data: T): void {
@@ -33,16 +45,27 @@ class LinkedList<T> {
             next: null
         };
         this.tail = this.tail.next;
+        this.size++;
+    }
+
+    toArray(): T[] {
+        const result = Array(this.size);
+        let node = this.head;
+        for (let i = 0; i < this.size; i++) {
+            result[i] = node.data;
+            node = node.next;
+        }
+        return result;
     }
 }
 
-type LinkedListNode<T> = {
+export type LinkedListNode<T> = {
     previous: LinkedListNode<T> | null;
     data: T;
     next: LinkedListNode<T> | null;
 }
 
-const ascii_list: LinkedList<string> = new LinkedList(String.fromCharCode(0));
+export const ascii_list: LinkedList<string> = new LinkedList(String.fromCharCode(0));
 
 for (let i = 1; i < 0xFF; i++) {
     ascii_list.append(String.fromCharCode(i));
